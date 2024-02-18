0249ecca6bb03e67a18022e3a23dc1258c4e21ba
Chry1911 <christian.cortese.cc@gmail.com>
2024-02-18 18:35:03 +0100

    added project section

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import {useEffect} from 'react'
 import Navbar from './components/Navbar';
 import Hiro from './components/Hiro';
 import Skills from './components/Skills'
-import Honors from './components/Honors';
 import Certs from './components/Certs';
 import Footer from './components/Footer';
 import AOS from 'aos';
@@ -19,6 +18,7 @@ function App() {
     <div className="px-6 lg:px-20 xl:px-36 bg-dark-500">
       <Navbar />
       <Hiro />
+      <Certs />
       <Skills />
       <Footer />
     </div>
